9723265193e2054aab751e95d66e35f3b9828f42
Pallavi20241 <pikudjadhav@gmail.com>
2024-05-04 01:54:14 +0530

    Array string

---
+++ src/ArrayCollection/ArrayaString.java
@@ -0,0 +1,14 @@
+package ArrayCollection;
+
+public class ArrayaString {//basic array String programme
+
+	public static void main(String[] args) {
+		String flowers[]=new String[4];
+		
+		flowers[0]="Rose";
+		flowers[1]="Marigold";
+		flowers[2]="Lily";
+		flowers[3]="Sunflower";
+	}
+
+}
--- src/java_programme_basic/LeapYear.java
+++ src/java_programme_basic/LeapYear.java
@@ -3,8 +3,9 @@ package java_programme_basic;
 public class LeapYear {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
+	
 	}
 
 }
